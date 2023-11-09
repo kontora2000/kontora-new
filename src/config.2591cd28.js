7901c4896076da45e13e1cd4e83ed15eb5c34d5b
stasmyhero <es@finfort.ru>
2023-11-09 23:11:40 +0300

    Updates

--- src/config.2591cd28.js
+++
@@ -1,1 +0,0 @@
-import{C as p,D as o,A as i}from"./entry.55cd2a0e.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
---
+++ src/config.bd5b675d.js
@@ -0,0 +1,1 @@
+import{C as p,D as o,A as i}from"./entry.99ffd6fd.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
